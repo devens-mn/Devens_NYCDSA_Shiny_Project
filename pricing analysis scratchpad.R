92965765202be4cc1d8ed29d160ad8d459805a8c
devens-mn <dadevens@gmail.com>
2020-05-07 14:26:55 -0500

    scratchpad.R - updated with joining user data, added logistic regression

--- pricing analysis scratchpad.R
+++ pricing analysis scratchpad.R
@@ -1,6 +1,7 @@
 # create days and month
 library(lubridate)
 # convert string to date
+
 promo$date = mdy(promo$date)
 
 # convert to day and month
@@ -60,9 +61,86 @@ ggplot(g) + geom_col(aes(x = source, y = winpct)) + geom_point(aes(x = source,
             sec.axis = sec_axis(~.*1600000, name = 'no visits')) + coord_flip()
             + theme_classic()
 
+# figure out logistic regression
+# clean out nas to make the model cleaner
+cleanpromo = promo %>% filter(., !nas)
 
+# join user data to clean promo and call it userclean
+userclean = left_join(cleanpromo, user, by = "user_id")
+summary(userclean)
 
+# count to figure out if some levels of factors didn't get same number of 0s
+xtabs(~ book + day, userclean) # 1s equally distributed
+xtabs(~ booked + day, userclean) # 1s equally distributed
+xtabs(~ booked + month, userclean) #not enough books in March, July
+xtabs(~ booked + state, userclean)
 
+# convert characters to factors
+userclean$source = as.factor(userclean$source)
+userclean$device = as.factor(userclean$device)
+userclean$day = as.factor(userclean$day)
+userclean$state = as.factor(userclean$state)
+str(userclean) # confirm that we got factors where we wanted
+summary(userclean)
 
+# clean NAs from states
+userclean$statenas = is.na(userclean$state)
+userclean = userclean %>% filter(., !statenas)
+summary(userclean)
+
+# identify rows with ones, other set with zeros
+input_ones <- userclean[which(userclean$booked == 1), ] 
+input_zeros <- userclean[which(userclean$booked == 0), ] 
+
+# sample row numbers from 1 rows and 0 rows
+set.seed(1)
+input_ones_training_rows = sample(1:nrow(input_ones), 0.5*nrow(input_ones))
+input_zeros_training_rows = sample(1:nrow(input_zeros), 0.5*nrow(input_ones))
+
+# use those rows to create training matrix for 1s and 0s
+training_ones = input_ones[input_ones_training_rows, ] 
+training_zeros = input_zeros[input_zeros_training_rows, ] 
+trainingData = rbind(training_ones, training_zeros) 
+
+# create test data from data not sampled into test
+test_ones = input_ones[-input_ones_training_rows, ]
+test_zeros = input_zeros[-input_zeros_training_rows, ]
+testData = rbind(test_ones, test_zeros)
+
+#create logistic model
+logitmodeltest = glm(booked ~ source + device + price + day + state, 
+          data = trainingData, family = 'binomial'(link = 'logit'))
+summary(logitmodeltest)
+
+# got rid of day and device, since they weren't significant
+logitmodeltest2 = glm(booked ~ source + price, data = 
+          trainingData, family = 'binomial'(link = 'logit'))
+summary(logitmodeltest2)
+
+# check VIF, I think need to load InformationValue
+vif(logitmodeltest2)
+
+# test model on test data, create vector of 'predicted'
+predicted = plogis(predict(logitmodeltest2, testData)) 
+summary(predicted)
+
+# set optimal cutoff value for deciding whether a prob is a 1 or 0
+optCutOff = optimalCutoff(testData$booked, predicted)[1]
+
+# misclassification
+misClassError(testData$booked, predicted, threshold = optCutOff)
+
+# ROC plot
+plotROC(testData$booked, predicted)
+
+# concordance
+Concordance(testData$booked, predicted)
+
+# sensitivity and specificity
+sensitivity(testData$booked, predicted, threshold = optCutOff)
+specificity(testData$booked, predicted, threshold = optCutOff)
+
+# confusion matrix, columns are actuals and rows are predicted
+confusionMatrix(testData$booked, predicted, threshold = optCutOff)
 
 
